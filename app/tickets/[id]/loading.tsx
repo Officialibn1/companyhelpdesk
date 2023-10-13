68953476528abd9322b0c2bf73e57f455dff979d
ibn <officialibn001@gmail.com>
2023-10-13 11:59:58 +0100

    Completed ticket loading ui

--- app/tickets/[id]/loading.tsx
+++ app/tickets/[id]/loading.tsx
@@ -3,7 +3,21 @@ import React from 'react'
 const TicketLoading = () => {
   return (
     <main>
-      
+      <div className="ticketsDetails">
+        <div className="loadingUi"></div>
+        <div className="loadingUi"></div>
+        <div className="loadingUi"></div>
+
+        <div className='loadingDiv'>
+            <div className="loadingUiSm"></div>
+            <div className="loadingUiSm"></div>
+            <div className="loadingUiSm"></div>
+            <div className="loadingUiSm"></div>
+            <div className="loadingUiSm"></div>
+            <div className="loadingUiSm"></div>
+            <div className="loadingUiSm"></div>
+        </div>
+      </div>
     </main>
   )
 }
